24b3cf606c52ad772f06b2cf34f3624938ceb8f1
Georgii Savin <gsavin@devexperts.com>
2021-02-18 20:44:55 +0300

    msg fix

--- lab1/src/test/kotlin/ru/itmo/CosTest.kt
+++ lab1/src/test/kotlin/ru/itmo/CosTest.kt
@@ -7,46 +7,46 @@ class CosTest {
 
     @Test
     fun `test 0 value`(){
-        assertEquals(1.0, Fun.cos(0.0), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(1.0, Fun.cos(0.0), Fun.PRECISION)
     }
 
     @Test
     fun `test PI value`(){
-        assertEquals(-1.0, Fun.cos(Math.PI), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(-1.0, Fun.cos(Math.PI), Fun.PRECISION)
     }
 
     @Test
     fun `test -PI value`(){
-        assertEquals(-1.0, Fun.cos(-Math.PI), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(-1.0, Fun.cos(-Math.PI), Fun.PRECISION)
     }
 
     @Test
     fun `test 0,5PI value`(){
-        assertEquals(0.0, Fun.cos(0.5 * Math.PI), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(0.0, Fun.cos(0.5 * Math.PI), Fun.PRECISION)
     }
 
     @Test
     fun `test -0,5PI value`(){
-        assertEquals(0.0, Fun.cos(-0.5 * Math.PI), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(0.0, Fun.cos(-0.5 * Math.PI), Fun.PRECISION)
     }
 
     @Test
     fun `test 0,33333PI value`(){
-        assertEquals(0.5, Fun.cos(0.33333 * Math.PI), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(0.5, Fun.cos(0.33333 * Math.PI), Fun.PRECISION)
     }
 
     @Test
     fun `test -0,33333PI value`(){
-        assertEquals(0.5, Fun.cos(-0.33333 * Math.PI), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(0.5, Fun.cos(-0.33333 * Math.PI), Fun.PRECISION)
     }
     @Test
     fun `test 0,66666666PI value`(){
-        assertEquals(-0.5, Fun.cos(0.66666666 * Math.PI), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(-0.5, Fun.cos(0.66666666 * Math.PI), Fun.PRECISION)
     }
 
     @Test
     fun `test -0,66666666PI value`(){
-        assertEquals(-0.5, Fun.cos(-0.66666666 * Math.PI), Fun.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
+        assertEquals(-0.5, Fun.cos(-0.66666666 * Math.PI), Fun.PRECISION)
     }
 
 }
